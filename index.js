dad754716817aaa4a90169c50118b5b34745db45
ortigaliev <ortigaliyevm@gmail.com>
2023-09-20 23:25:46 +0900

    install uuid pack

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
 /* const moment  = require('moment');
 
-
 setInterval(() => {
   const time = moment().format();
   console.log(`Hozirgi vaqt ${time}`);
-
 }, 5000); */
 
 
-// inquirer package - eng ko'p ishlatiladigan package bu bizga terminal bn bog'liq kiritish chiqarish. Bugungi misolda user input qilishi uchun ishlatamiz!
 
+// inquirer package - eng ko'p ishlatiladigan package bu bizga terminal bn bog'liq kiritish chiqarish. Bugungi misolda user input qilishi uchun ishlatamiz!
 /* const inquirer = require('inquirer');
 inquirer
 .prompt([{type: 'input', name: 'raqam', message: 'raqamni kiriting? '}])
@@ -18,7 +16,13 @@ inquirer
 })
 .catch ((err) =>console.log(err)); */
 
-//validator package - bu malumotlarni validat qilib beradi. Install qilamiz
+
+
+/* //validator package - bu malumotlarni validat qilib beradi. Install qilamiz
 const validator = require('validator');
-const test = validator.isEmail('foo@bar.com');
-console.log('test', test);
+//const test = validator.isEmail('foo@bar.com');
+//const test = validator.isInt('100');// integerni tekshiradi
+const test = validator.isIP('121.139.161.'); //ipni tekshiradi
+console.log('test', test); // bu aynan emailimizni @ belgisi borligini va .com bo'lishini tekshiradi. Buni tekshirish uchun algoratim yozib o'tirmasdan shuni tayyor ishlatish mumkin */
+
+
