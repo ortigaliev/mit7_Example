5602d483d9300046dfc94d83c4a7949efd661ac4
ortigaliev <ortigaliyevm@gmail.com>
2023-09-20 23:02:12 +0900

    install inquirer pack and example prompt

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
-const moment  = require('moment');
+/* const moment  = require('moment');
 
 
 setInterval(() => {
   const time = moment().format();
   console.log(`Hozirgi vaqt ${time}`);
 
-}, 5000);
+}, 5000); */
+
+// inquirer package - eng ko'p ishlatiladigan package bu bizga terminal bn bog'liq kiritish chiqarish. Bugungi misolda user input qilishi uchun ishlatamiz!
+
+const inquirer = require('inquirer');
+inquirer
+.prompt([{type: 'input', name: 'raqam', message: 'raqamni kiriting? '}])
+.then((answer) => {
+  console.log('Men kiritgan raqamning qiymati:', answer.raqam);
+})
+.catch ((err) =>console.log(err));
