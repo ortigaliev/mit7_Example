9c155d5017806b0631fe6e1305d0513c82de15dc
ortigaliev <ortigaliyevm@gmail.com>
2023-09-20 23:09:02 +0900

    install validator pack

--- index.js
+++ index.js
@@ -7,12 +7,15 @@ setInterval(() => {
 
 }, 5000); */
 
+
 // inquirer package - eng ko'p ishlatiladigan package bu bizga terminal bn bog'liq kiritish chiqarish. Bugungi misolda user input qilishi uchun ishlatamiz!
 
-const inquirer = require('inquirer');
+/* const inquirer = require('inquirer');
 inquirer
 .prompt([{type: 'input', name: 'raqam', message: 'raqamni kiriting? '}])
 .then((answer) => {
   console.log('Men kiritgan raqamning qiymati:', answer.raqam);
 })
-.catch ((err) =>console.log(err));
+.catch ((err) =>console.log(err)); */
+
+//validator package - bu malumotlarni validat qilib beradi. Install qilamiz
