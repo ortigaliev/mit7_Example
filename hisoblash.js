27e2d58788db782bfc7df865aa1a490af670e0ff
ortigaliev <ortigaliyevm@gmail.com>
2023-09-21 20:42:29 +0900

    file packages

---
+++ hisoblash.js
@@ -0,0 +1,14 @@
+let hisobla ={};
+
+hisobla.kupaytirish = (a, b) => {
+  return a * b;
+};
+hisobla.bulish = (a, b) => {
+  return a / b;
+};
+hisobla.qushish = (a, b) => {
+  return a + b;
+};
+hisobla.ayrish = (a, b) => {
+  return a - b;
+};
